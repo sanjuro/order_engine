f0043e19ed97f43a183bd80bb9a58b4695fcb3ee
sanjuro <shad6ster@gmail.com>
2013-01-30 11:06:40 +0200

    Updated users

--- app/apis/users.rb
+++ app/apis/users.rb
@@ -28,6 +28,23 @@ class Users < Grape::API
       logger.info "Creating new customer"
       SignupCustomerContext.call(params)
     end
+
+    desc "Authenticates a Store User"
+    params do
+      requires :username, :type => String, :desc => "Username to authenticate."
+      requires :password, :type => String, :desc => "Password to authenticate."
+    end
+    post '/authenticate_store' do
+      authenticated_user
+      logger.info "Authenticating new InStore Device with Username: #{params[:username]}"
+
+      store_user = StoreUser.by_username(params[:username]).first
+      if store_user.nil?
+
+      else
+        AuthenticateStoreContext.call(store_user, params[:password])
+      end
+    end
   end
   
 end
