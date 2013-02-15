d7a740d96e03c711cb22900996e455798c572a84
sanjuro <shad6ster@gmail.com>
2013-02-15 16:19:16 +0200

    Modified get new orders by created at asc

--- app/contexts/get_new_store_orders_context.rb
+++ app/contexts/get_new_store_orders_context.rb
@@ -14,7 +14,7 @@ class GetNewStoreOrdersContext
   end
 
   def call
-    orders = Order.select("orders.*, users.first_name, users.last_name, users.email, users.mobile_number").sent_to_store.joins(:customer).by_store(@store.id)
+    orders = Order.select("orders.*, users.first_name, users.last_name, users.email, users.mobile_number").sent_to_store.joins(:customer).by_store(@store.id).order('created_at asc')
 
     orders_return = Hash.new
 
