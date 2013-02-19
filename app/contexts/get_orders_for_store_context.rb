20a9304f887db79b703dfaca3cc8125690124990
sanjuro <shad6ster@gmail.com>
2013-02-19 23:31:22 +0200

    Added missing file

---
+++ app/contexts/get_orders_for_store_context.rb
@@ -0,0 +1,30 @@
+# Context to retrieve a specific store
+#
+# Author::    Shadley Wentzel
+
+class GetOrdersForStoreContext
+  attr_reader :store_id, :state
+
+  def self.call(store_id, state)
+    GetOrdersForStoreContext.new(store_id, state).call
+  end
+
+  def initialize(store_id, state)
+    @store_id, @state = store_id, state
+  end
+
+  def call
+    	# return favourties
+   	 	store = Store.find(@store_id)
+
+    	orders = Order.by_store(@store_id).by_state(state).created_on(Date.today)
+
+	    orders_return = Hash.new
+
+	    orders.each do |order|
+	      orders_return[order.id] = order.format_for_web_serivce 
+	    end
+
+	    orders_return
+  end
+end
