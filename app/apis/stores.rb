4b9213db02076264965313581764c532416e4f93
sanjuro <shad6ster@gmail.com>
2013-02-25 22:35:45 +0200

    Updated new orders function

--- app/apis/stores.rb
+++ app/apis/stores.rb
@@ -5,7 +5,7 @@ class Stores < Grape::API
   
   resource 'stores' do
 
-    # curl -i -H "Accept: application/json" http://127.0.0.1:9000/api/v1/stores/1/orders_for_todat=y?authentication_token=CXTTTTED2ASDBSD3
+    # curl -i -H "Accept: application/json" http://107.22.211.58:9000/api/v1/stores/1/new_orders?authentication_token=AXSSSSED2ASDASD2
     # curl -i -H "Accept: application/json" http://107.22.211.58:9000/api/v1/stores/search/kauai
     # curl -v -H 'Accept: application/json' -X POST -d '{"authentication_token": "AXSSSSED2ASDASD2",}' http://107.22.211.58:9000/api/v1/orders/1/cancel
     # curl -H 'Accept: application/json' -X POST -d '{"authentication_token": "CXTTTTED2ASDBSD4", "query_term": "kauai", "latitude": "-33.922796", "longitude": "18.420808"}' 'http://127.0.0.1:9000/api/v1/stores/search' -v
