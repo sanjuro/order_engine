ea93ec7b636bf034027f8391652fcd8a485cf06f
sanjuro <shad6ster@gmail.com>
2013-02-28 19:37:42 +0200

    Updated Order email

--- action_mailer_configuration.rb
+++ action_mailer_configuration.rb
@@ -1,14 +1,18 @@
 require 'action_mailer'
 
+# ActionMailer::Base.smtp_settings = {
+#   :address        		=> 'mail.vosto.co.za', # default: localhost
+#   :port           		=> '25',                  # default: 25
+#   :domain         		=> 'vosto.co.za',
+#   :user_name      		=> 'info@vosto.co.za',
+#   :password      		=> 'R@d6hi@..',
+#   :authentication       => "plain",       # :plain, :login or :cram_md5
+#   :enable_starttls_auto => true,
+#   :openssl_verify_mode  => 'none'              
+# }
 ActionMailer::Base.smtp_settings = {
-  :address        		=> 'mail.vosto.co.za', # default: localhost
-  :port           		=> '25',                  # default: 25
-  :domain         		=> 'vosto.co.za',
-  :user_name      		=> 'info@vosto.co.za',
-  :password      		=> 'R@d6hi@..',
-  :authentication       => "plain",       # :plain, :login or :cram_md5
-  :enable_starttls_auto => true,
-  :openssl_verify_mode  => 'none'              
+  :address        		=> 'localhost', # default: localhost
+  :port           		=> '25',                  # default: 25            
 }
 
 ActionMailer::Base.view_paths = Dir[File.dirname(__FILE__) + '/app/views/']
