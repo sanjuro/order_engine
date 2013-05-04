3203ff6adcb56badaceaeef4e0a67fad7aaee176
sanjuro <shad6ster@gmail.com>
2013-05-04 22:17:55 +0200

    Updated Register new device serivce

--- app/apis/devices.rb
+++ app/apis/devices.rb
@@ -6,7 +6,7 @@ class Devices < Grape::API
   resource 'devices' do
 
     # curl -i -H "Accept: application/json" http://localhost:9000/api/v1/orders/page/1?authentication_token=AXSSSSED2ASDASD6
-    # curl -i -H "Accept: application/json" -X POST -d '{"authentication_token": "AXSSSSED2ASDASD2","device":{"device_type":"android", "device_identifier":"103edb7d8c4e3669", "device_token": "APA91bFgMkYWLlF68P9OkC8YhE7dPHjDWYWrRJDAolkSa19CS1jZ_79GkkAwfq3QWIimpKI0Otkf_u89vVkZdFH0tlXkoKdusvR7RazE7pbTx-mt_UWfg-URY_ebS7hlCS2SYpIH9tj1uAzMLAH_zoA6bZ6bJ9TOpQ"}}' http://127.0.0.1:9000/api/v1/devices/register
+    # curl -i -H "Accept: application/json" -X POST -d '{"authentication_token": "fa36bfe1887b23db203ab3e836517c51","device":{"device_type":"android", "device_identifier":"103edb7d8c4e3669", "device_token": "APA91bFgMkYWLlF68P9OkC8YhE7dPHjDWYWrRJDAolkSa19CS1jZ_79GkkAwfq3QWIimpKI0Otkf_u89vVkZdFH0tlXkoKdusvR7RazE7pbTx-mt_UWfg-URY_ebS7hlCS2SYpIH9tj1uAzMLAH_zoA6bZ6bJ9TOpQ"}}' http://127.0.0.1:9000/api/v1/devices/register
     # curl -i -H "Accept: application/json" -X POST -d '{"authentication_token": "DXTTTTED2ASDBSD3","device":{"device_type":"blackberry", "device_identifier":"PP-oad11-123456", "device_token": "12345"}}' http://107.22.211.58:9000/api/v1/devices/register -v
 
     desc "Register new Device"
--- spec/apis/devices_spec.rb
+++ spec/apis/devices_spec.rb
@@ -7,7 +7,7 @@ describe Devices do
     Devices
   end
 
-    context "POST /api/v1/devices/:id/status" do
+    context "POST /api/v1/devices/register" do
 
       context "GIVEN registering a new device" do
 
@@ -32,6 +32,37 @@ describe Devices do
           last_response.body.empty?.should == false
         end
       end
+
+      context "GIVEN registering an old device" do
+
+        before :each do   
+          @request_payload = {
+            authentication_token: "CXTTTTED2ASDBSD4",
+            device: {
+              device_type: 1,
+              device_identifier: "PP-oad11-1234",
+              device_token: "666666666",
+              device_type: 'blackberry'
+            }
+          }
+          post '/api/v1/devices/register',  @request_payload.to_json
+        end
+
+        it 'SHOULD return a 200' do          
+          last_response.status.should == 201
+        end
+
+        it 'SHOULD return the status of the order' do
+          last_response.body.empty?.should == false
+        end
+
+        it 'SHOULD update the old device record' do
+          device = Device.find_by_device_identifier("PP-oad11-1234")
+          device.count.should == 1
+        end
+      end
+
     end  
+
   
 end
