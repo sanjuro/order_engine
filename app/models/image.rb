7cd385d35cd0891db2f7e2a70532ab6aac19f584
sanjuro <shad6ster@gmail.com>
2013-05-15 23:59:57 +0200

    Updated image paths

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,7 +11,7 @@ class Image < Asset
   has_attached_file :attachment,
                     :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
                     :default_style => :product,
-                    :url => '/:viewable_type/:id/:style/:basename.:extension',
+                    :url =>  "/:id/:style/:basename.:extension",
                     :path => 'http://m.vosto.co.za/public/:viewable_type/:id/:style/:basename.:extension',
                     :convert_options => { :all => '-strip -auto-orient' }
   # save the w,h of the original image (from which others can be calculated)
