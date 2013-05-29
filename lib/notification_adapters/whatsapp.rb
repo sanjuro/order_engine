8a8323782a66bf56ffa1c0eec21a16d303262e8d
sanjuro <shad6ster@gmail.com>
2013-05-29 14:50:25 +0200

    Added resuqe

---
+++ lib/notification_adapters/whatsapp.rb
@@ -0,0 +1,21 @@
+require "pushmeup"  
+  
+module Notification  
+  module Adapters  
+    module Whatsapp  
+      extend self  
+      def send(destination, message) 
+        # p message   
+        if destination.kind_of?(Array)
+          destination.each do |number|
+            command = "php #{RESQUE['root_dir']}/whatsapp/vosto_sender.php #{number} '#{message}'"     
+            system(command)          
+          end
+        else
+          command = "php #{RESQUE['root_dir']}/whatsapp/vosto_sender.php #{destination} '#{message}'"     
+          system(command)
+        end 
+      end
+    end  
+  end  
+end  
