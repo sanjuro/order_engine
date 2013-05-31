ee9f770eaeeda810ce42831d85edad2bc82cac87
sanjuro <shad6ster@gmail.com>
2013-05-31 15:52:16 +0200

    Added ios notification support

--- app/workers/notification_sender.rb
+++ app/workers/notification_sender.rb
@@ -9,19 +9,6 @@ class NotificationSender
     # get all devices for the store
     order.store.devices.each do |device|
       case device.device_type
-      when 'ios'
-        p "ORDER ID #{order_id}:Sending ios notification"
-        Notification.adapter = 'ios'
-
-        message = Hash.new
-        message[:order_id] = self.id
-        message[:msg] = "Your order: #{order_number} has been received and will be ready in #{self.time_to_ready} minutes."
-        message[:updated_at] = self.updated_at
-        message[:store_order_number] = self.store_order_number
-        message[:state] = self.state
-        message[:time_to_ready] = self.time_to_ready
-
-        Notification.send(device.device_token, message)
       when 'android'
         p "ORDER ID #{order_id}:Sending android notification"
         Notification.adapter = 'android'
