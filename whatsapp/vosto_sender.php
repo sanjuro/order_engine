686a2044b95739611995d1ea8702a33dcad5e99b
sanjuro <shad6ster@gmail.com>
2013-06-13 17:14:51 +0200

    Updated issue on whatsapp

--- whatsapp/vosto_sender.php
+++ whatsapp/vosto_sender.php
@@ -1,5 +1,6 @@
 <?php
     #  php vosto_sender.php "27833908314" "New VOSTO ORDER  Customer: Shadley Wentzel, 0833908314  Order: #123, Collection Spurs Famous Pork Ribs 1 X 89.95"
+    #  php vosto_sender.php "27848013237" "Free Ticket: Mavericks Town, just say ken sent me."
     require "whatsprot.class.php";
 
     $fromMobNumber = "27745532622"; //ENTER YOUR GOOGLEVOICENUMBER HERE
@@ -21,8 +22,6 @@
      * @param $message string The text message.
      */
      $w->Message($destinationPhone, $message);
-     
-    exit;
 
     /**
      * Wait for message delivery notification.
