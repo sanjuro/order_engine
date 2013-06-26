e045615e19f8dc65d52ca9f267ed1fb7084663e9
sanjuro <shad6ster@gmail.com>
2013-06-26 23:32:21 +0200

    Updated Whatsapp

--- lib/notification_adapters/whatsapp.rb
+++ lib/notification_adapters/whatsapp.rb
@@ -4,15 +4,16 @@ module Notification
   module Adapters  
     module Whatsapp  
       extend self  
-      def send(destination, message) 
-        # p message   
+      def send(destination, message)   
         if destination.kind_of?(Array)
           destination.each do |number|
-            command = "php /Users/macbookpro/Sites/vosto_order/whatsapp/vosto_sender.php #{number} '#{message}'"     
+            # command = "php /Users/macbookpro/Sites/vosto_order/whatsapp/vosto_sender.php #{number} '#{message}'" 
+            command = "php /usr/share/vosto_order/current/whatsapp/vosto_sender.php #{number} '#{message}'"         
             system(command)          
           end
         else
-          command = "php /Users/macbookpro/Sites/vosto_order//whatsapp/vosto_sender.php #{destination} '#{message}'"     
+          # command = "php /Users/macbookpro/Sites/vosto_order//whatsapp/vosto_sender.php #{destination} '#{message}'"  
+          command = "php /usr/share/vosto_order/current/whatsapp/vosto_sender.php #{number} '#{message}'"   
           system(command)
         end 
       end
