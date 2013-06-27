80fe01f787e4bbe31be08c736697b1bded00664b
sanjuro <shad6ster@gmail.com>
2013-06-27 15:53:18 +0200

    Updated whatsapp notification

--- lib/notification_adapters/whatsapp.rb
+++ lib/notification_adapters/whatsapp.rb
@@ -8,7 +8,7 @@ module Notification
         if destination.kind_of?(Array)
           destination.each do |number|
             # command = "php /Users/macbookpro/Sites/vosto_order/whatsapp/vosto_sender.php #{number} '#{message}'"  
-            command = "php /usr/share/vosto_order/current/whatsapp/vosto_sender.php #{destination} '#{message}'"         
+            command = "php /usr/share/vosto_order/current/whatsapp/vosto_sender.php #{number} '#{message}'"         
             system(command)          
           end
         else
