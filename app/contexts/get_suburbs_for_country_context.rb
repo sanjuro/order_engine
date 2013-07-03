d9f8fa5d63a292da575d39550270a4a740acdefa
sanjuro <shad6ster@gmail.com>
2013-07-03 12:58:54 +0200

    Updated suburbs service

--- app/contexts/get_suburbs_for_country_context.rb
+++ app/contexts/get_suburbs_for_country_context.rb
@@ -19,7 +19,7 @@ class GetSuburbsForCountryContext
 
     suburbs_return = Array.new
     suburbs.each do |suburb|
-      suburbs_return << { "id" => suburb.id, "name" => suburb.name, "state_id" => suburb.state.id, "state_name" => suburb.state.name }
+      suburbs_return << { "id" => suburb.id, "name" => suburb.name }
     end
     suburbs_return
   end
