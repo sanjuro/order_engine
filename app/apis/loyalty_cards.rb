47e9f25175f4272b6df95dd2e385406e33de2d11
sanjuro <shad6ster@gmail.com>
2013-10-06 20:48:45 +0200

    Updated loyalty card api

--- app/apis/loyalty_cards.rb
+++ app/apis/loyalty_cards.rb
@@ -5,7 +5,22 @@ class LoyaltyCards < Grape::API
   
   resource 'loyalty_cards' do
 
-    # curl -i -H "Accept: application/json" http://107.22.211.58:9000/api/v1/loyalty_cards/by_customer?authentication_token=d1b01126294db97ad5588aa50ae90952 -v
+    # curl -i -H "Accept: application/json" http://127.0.0.1:9000/api/v1/loyalty_cards/1?authentication_token=d1b01126294db97ad5588aa50ae90952 -v
+
+    desc "Returns all loyalty cards for a customer"
+    get "/by_customer" do  
+      logger.info "Retrieved all loyalty cards for a customer"
+      authenticated_user
+      logger.info "Authenticated User: #{current_user.full_name}"
+
+      user = User.find(current_user.id)
+      if !user.nil?
+        GetLoyaltyCardsForCustomerCostContext.call(user) 
+      else
+        error!({ "error" => "Customer error", "detail" =>  "Customer could not be found" }, 400)  
+      end
+    end
+
 
     desc "Retrieve a specific loyalty card"
     params do
@@ -21,20 +36,6 @@ class LoyaltyCards < Grape::API
         GetLoyaltyCardContext.call(loyalty_card.id)
       else
         error!({ "error" => "Loyalty Card error", "detail" =>  "Loyalty Card could not be found" }, 400)  
-      end
-    end
-
-    desc "Returns all loyalty cards for a customer"
-    get "/by_customer" do  
-      logger.info "Retrieved all loyalty cards for a customer"
-      authenticated_user
-      logger.info "Authenticated User: #{current_user.full_name}"
-
-      user = User.find(current_user.id)
-      if !user.nil?
-        GetLoyaltyCardsForCustomerCostContext.call(user) 
-      else
-        error!({ "error" => "Customer error", "detail" =>  "Customer could not be found" }, 400)  
       end
     end
 
